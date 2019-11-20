dd9b2adc8429a1d079ed9de251ba8a269feaa0c3
Takuma Aoki <taku1106aoki@gmail.com>
2019-11-20 10:54:12 +0900

    front end

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :experiences
   resources :skills
   resources :messages
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
